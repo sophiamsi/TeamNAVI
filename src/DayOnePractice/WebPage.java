ab9541e24dc66c16c09e754b3dbdb56e5989c383
sophiamsi <sophia.malett@gmail.com>
2022-12-23 10:12:11 -0600

    Project was updated

--- src/DayOnePractice/WebPage.java
+++ src/DayOnePractice/WebPage.java
@@ -3,5 +3,6 @@ package DayOnePractice;
 public class WebPage {
     public static void main(String[] args) {
         System.out.println("Our first practice together ");
+        // we created different pages
     }
 }
